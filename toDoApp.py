ff412794d9d69bfd14d33766e552d41723439770
Balázs Máté <balazsm86@gmail.com>
2016-10-17 21:27:38 +0200

    Add Input(), ListItems(), AddItems(), MarkItems() and ArchiveItems() functions

--- toDoApp.py
+++ toDoApp.py
@@ -1,12 +1,44 @@
-def Input():
-    Command = input("Please specify a command [list, add, mark, archive]: ")
-    return Command
+# ========== Functions ==========
 
 def ListItems():
+    try:
+        file = open("to_do_list.txt", "r")          # Open the file in 'append' mode
+        Content = file.read()
+        print("You saved the following to-do items:")
+        print(Content)
+        file.close()        
+    except:
+        file.close()
 
 def AddItems():
+    try:
+        file = open("to_do_list.txt", "a")          # Open the file in 'append' mode
+        file.write(input("Add an item: "))
+        file.close()
+        print("Item added.")       
+    except:
+        file.close()
 
 def MarkItems():
 
 def ArchiveItems():
 
+
+def InputCommand():
+    Command = input("Please specify a command [list, add, mark, archive]: ")
+    if Command == "list" or "List":
+        ListItems()
+    elif Command == "add" or "Add":
+        AddItems()
+    elif Command == "mark" or "Mark":
+        MarkItems()
+    elif Command == "archive" or "Archive":
+        ArchiveItems()
+    else:
+        print("Invalid command!")
+    return Command
+
+
+# ========== Main ==========
+
+InputCommand()
