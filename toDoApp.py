181879034f72a56c26348a0541cb8177ba63e98d
Balázs Máté <balazsm86@gmail.com>
2016-10-17 20:54:29 +0200

    Add Input(), ListItems(), AddItems(), MarkItems() and ArchiveItems() functions

---
+++ toDoApp.py
@@ -0,0 +1,12 @@
+def Input():
+    Command = input("Please specify a command [list, add, mark, archive]: ")
+    return Command
+
+def ListItems():
+
+def AddItems():
+
+def MarkItems():
+
+def ArchiveItems():
+
