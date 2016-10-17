84fb8eda130a9e7afa752bd3d55b9cc60ac7fbd8
Balázs Máté <balazsm86@gmail.com>
2016-10-17 23:10:42 +0200

    Created some functions

--- toDoApp.py
+++ toDoApp.py
@@ -1,42 +1,61 @@
 # ========== Functions ==========
 
+def CreateFile():
+    try:
+        file = open("to_do_list.txt", "a")              # If the file does not exist, it creates a new file
+        file.close()
+    except:
+        file.close()
+
+def CountFileLines():
+    file = open("to_do_list.txt", "r")
+    NumOfLines = len(file.readlines())                  # Count the number of lines in the file
+    file.close()
+    return NumOfLines
+
 def ListItems():
     try:
-        file = open("to_do_list.txt", "r")          # Open the file in 'append' mode
-        Content = file.read()
+        file = open("to_do_list.txt", "r")              # Open the file in 'read' mode
+        Content = file.read()        
         print("You saved the following to-do items:")
         print(Content)
         file.close()        
     except:
         file.close()
 
-def AddItems():
+def AddItems(numofilnes):
     try:
-        file = open("to_do_list.txt", "a")          # Open the file in 'append' mode
-        file.write(input("Add an item: "))
+        file = open("to_do_list.txt", "a")              # Open the file in 'append' mode        
+        NewContent = input("Add an item: ")
+        file.write(str(numofilnes+1) + ". " + "[ ] " + str(NewContent) + "\n")
         file.close()
-        print("Item added.")       
+        print("Item added.")
+        file.close()
     except:
         file.close()
 
-def MarkItems():
+#def MarkItems():
 
-def ArchiveItems():
+
+#def ArchiveItems():
 
 
 def InputCommand():
-    Command = input("Please specify a command [list, add, mark, archive]: ")
-    if Command == "list" or "List":
-        ListItems()
-    elif Command == "add" or "Add":
-        AddItems()
-    elif Command == "mark" or "Mark":
-        MarkItems()
-    elif Command == "archive" or "Archive":
-        ArchiveItems()
-    else:
-        print("Invalid command!")
-    return Command
+    CreateFile()
+    #Command = input("Please specify a command [list, add, mark, archive]: ")
+    while True:
+        NumOfLines = CountFileLines()
+        Command = input("Please specify a command [list, add, mark, archive]: ")
+        if Command == "list" or Command == "List":
+            ListItems()
+        elif Command == "add" or Command == "Add":
+            AddItems(NumOfLines)
+        elif Command == "mark" or Command == "Mark":
+            MarkItems()
+        elif Command == "archive" or Command == "Archive":
+            ArchiveItems()
+        else:
+            break
 
 
 # ========== Main ==========
